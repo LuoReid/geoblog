a24753ea00d0487d775b9e318e60f403cf4a2e3a
Luo Reid <luo.reid@gmail.com>
2019-03-17 18:41:28 +0800

    router

---
+++ src/components/GeoBlog.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="geo-blog">
+        //TODO
+    </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from './components/Login.vue'
+import GeoBlog from './components/GeoBlog.vue'
+import NotFound from './components/NotFound.vue'
+
 Vue.use(VueRouter)
 
-const routers = []
+const routers = [
+  { path: '/', name: 'home', component: GeoBlog, meta: { private: true } },
+  { path: 'login', name: 'login', component: Login },
+  { path: '*', component: NotFound },
+]
 
-const router = new VueRouter({ routers })
+const router = new VueRouter({
+  routers,
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      return { selector: to.hash }
+    }
+    return { x: 0, y: 0 }
+  }
+})
+
+//todo navigation guard
+
 export default router
